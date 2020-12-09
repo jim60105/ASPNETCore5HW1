1c042ac74cbbf9ffda67ce0e6a7f650700ec9c5a
陳鈞 <jim60105@gmail.com>
2020-12-10 01:34:31 +0800

    以 ContosoUniversity 資料庫為主要資料來源 透過 DB First 流程建立 EF Core 實體資料模型

---
+++ ASPNETCore5HW1/Models/VwDepartmentCourseCount.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ASPNETCore5HW1.Models
+{
+    public partial class VwDepartmentCourseCount
+    {
+        public int DepartmentId { get; set; }
+        public string Name { get; set; }
+        public int? CourseCount { get; set; }
+    }
+}
