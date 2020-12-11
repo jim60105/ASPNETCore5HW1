a58a418067f439bfbceac46b40838afa0d2d80a8
陳鈞 <jim60105@gmail.com>
2020-12-11 21:27:25 +0800

    針對 Departments 表格的 CUD 操作需用到預存程序 只有Insert成功

--- ASPNETCore5HW1/Controllers/DepartmentsController.cs
+++ ASPNETCore5HW1/Controllers/DepartmentsController.cs
@@ -1,41 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+using ASPNETCore5HW1.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using ASPNETCore5HW1.Models;
 using Omu.ValueInjecter;
 
-namespace ASPNETCore5HW1.Controllers
-{
+namespace ASPNETCore5HW1.Controllers {
     [Route("api/[controller]")]
     [ApiController]
-    public class DepartmentsController : ControllerBase
-    {
+    public class DepartmentsController : ControllerBase {
         private readonly ContosoUniversityContext db;
 
-        public DepartmentsController(ContosoUniversityContext context)
-        {
-            db = context;
-        }
+        public DepartmentsController(ContosoUniversityContext context) => db = context;
 
         // GET: api/Departments
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Department>>> GetDepartments()
-        {
-            return await db.Departments.ToListAsync();
-        }
+        public async Task<ActionResult<IEnumerable<Department>>> GetDepartments() => await db.Departments.ToListAsync();
 
         // GET: api/Departments/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Department>> GetDepartment(int id)
-        {
-            var department = await db.Departments.FindAsync(id);
+        public async Task<ActionResult<Department>> GetDepartment(int id) {
+            Department department = await db.Departments.FindAsync(id);
 
-            if (department == null)
-            {
+            if (department == null) {
                 return NotFound();
             }
 
@@ -50,8 +38,7 @@ namespace ASPNETCore5HW1.Controllers
             }
 
             Department department = await db.Departments.FindAsync(id);
-            department?.InjectFrom(departmentVM);
-            await db.SaveChangesAsync();
+            db.Departments.FromSqlInterpolated($"dbo.Department_Update {department.DepartmentId},{departmentVM.Name},{departmentVM.Budget},{departmentVM.StartDate},{departmentVM.InstructorId},{department.RowVersion}");
 
             return NoContent();
         }
@@ -59,33 +46,19 @@ namespace ASPNETCore5HW1.Controllers
         // POST: api/Departments
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-       public async Task<ActionResult<Department>> PostDepartment(Department departmentVM) {
-            Department department = new Department();
-            department.InjectFrom(departmentVM);
-
-            var entry = db.Departments.Add(department);
-            await db.SaveChangesAsync();
-
-            department =(Department) entry.GetDatabaseValues().ToObject();
-            return Created($"/api/Departments/{department.DepartmentId}",department);
+        public ActionResult<Department> PostDepartment(Department departmentVM) {
+            db.Departments.FromSqlInterpolated($"dbo.Department_Insert {departmentVM.Name},{departmentVM.Budget},{departmentVM.StartDate},{departmentVM.InstructorId}");
+            return Ok();
         }
 
         // DELETE: api/Departments/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteDepartment(int id) {
-            var department = await db.Departments.FindAsync(id);
-            if (department == null) {
-                return NotFound();
-            }
-
-            db.Departments.Remove(department);
-            await db.SaveChangesAsync();
-
-            return NoContent();
+            var department = await GetDepartment(id);
+            db.Departments.FromSqlInterpolated($"dbo.Department_Delete {department.Value.DepartmentId},{department.Value.RowVersion}");
+            return Ok();
         }
 
-        private bool DepartmentExists(int id) {
-            return db.Departments.Any(e => e.DepartmentId == id);
-        }
+        private bool DepartmentExists(int id) => db.Departments.Any(e => e.DepartmentId == id);
     }
 }
