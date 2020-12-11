063beb684b8068819fd54293ead994cf58b216f5
陳鈞 <jim60105@gmail.com>
2020-12-12 00:35:44 +0800

    (沒完成) 請修改 Course, Department, Person 表格，新增 DateModified 欄位(datetime)，並且這三個表格的資料透過 Web API 更新時，都要自動更新該欄位為更新當下的時間 (請新增資料庫移轉紀錄)

---
+++ ASPNETCore5HW1/Models/ContosoUniversityContext.Partial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace ASPNETCore5HW1.Models {
+    public partial class ContosoUniversityContext : DbContext {
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder) {
+            // 這不成功
+            SavingChanges+= delegate {
+                var entities = this.ChangeTracker.Entries();
+
+                foreach (var entry in entities) {
+                    if (entry.State == EntityState.Modified) {
+                        entry.CurrentValues.SetValues(new { DateModified = DateTime.Now });
+                    }
+                }
+            };
+        }
+    }
+}
