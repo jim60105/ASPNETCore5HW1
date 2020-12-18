6942cd8d60a4364650c590b7215210b6611610ef
陳鈞 <jim60105@gmail.com>
2020-12-18 11:40:48 +0800

    以Repository Pattern實做IsDeleted和DateModified (在PersonControler)

--- ASPNETCore5HW1/Controllers/PersonController.cs
+++ ASPNETCore5HW1/Controllers/PersonController.cs
@@ -1,41 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using ASPNETCore5HW1.Models;
+using Microsoft.AspNetCore.Mvc;
 using Omu.ValueInjecter;
+using Repository;
 
-namespace ASPNETCore5HW1.Controllers
-{
+namespace ASPNETCore5HW1.Controllers {
     [Route("api/[controller]")]
     [ApiController]
-    public class PeopleController : ControllerBase
-    {
-        private readonly ContosoUniversityContext db;
+    public class PeopleController : ControllerBase {
+        private readonly IsDeletedRepository<Person> repo;
 
-        public PeopleController(ContosoUniversityContext context)
-        {
-            db = context;
-        }
+        public PeopleController(IsDeletedRepository<Person> context) => repo = context;
+        private Person FindById(int id) => repo.FindByCondition(p => p.Id == id).FirstOrDefault();
 
         // GET: api/People
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Person>>> GetPeople()
-        {
-            return await db.People.ToListAsync();
-        }
+        public ActionResult<IEnumerable<Person>> GetPeople() => repo.FindAll().ToList();
 
         // GET: api/People/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Person>> GetPerson(int id)
-        {
-            var person = await db.People.FindAsync(id);
+        public ActionResult<Person> GetPerson(int id) {
+            Person person = FindById(id);
 
-            if (person == null)
-            {
+            if (person == null) {
                 return NotFound();
             }
 
@@ -44,14 +32,15 @@ namespace ASPNETCore5HW1.Controllers
 
         // PUT: api/People/5
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutPerson(int id, PersonEditVM personVM) {
-            if (!PersonExists(id)) {
+        public IActionResult PutPerson(int id, PersonEditVM personVM) {
+            Person person = FindById(id);
+            if (null == person) {
                 return NotFound();
             }
 
-            Person person = await db.People.FindAsync(id);
             person?.InjectFrom(personVM);
-            await db.SaveChangesAsync();
+            repo.Update(person); //必要，為了寫入DateModified
+            repo.SaveChanges();
 
             return NoContent();
         }
@@ -59,33 +48,29 @@ namespace ASPNETCore5HW1.Controllers
         // POST: api/People
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-       public async Task<ActionResult<Person>> PostPerson(PersonEditVM personVM) {
+        public ActionResult<Person> PostPerson(PersonEditVM personVM) {
             Person person = new Person();
             person.InjectFrom(personVM);
 
-            var entry = db.People.Add(person);
-            await db.SaveChangesAsync();
+            Microsoft.EntityFrameworkCore.ChangeTracking.EntityEntry<Person> entry = repo.Create(person);
+            repo.SaveChanges();
 
-            person =(Person) entry.GetDatabaseValues().ToObject();
-            return Created($"/api/People/{person.Id}",person);
+            person = (Person)entry.GetDatabaseValues().ToObject();
+            return Created($"/api/People/{person.Id}", person);
         }
 
         // DELETE: api/People/5
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeletePerson(int id) {
-            var person = await db.People.FindAsync(id);
+        public IActionResult DeletePerson(int id) {
+            Person person = FindById(id);
             if (person == null) {
                 return NotFound();
             }
 
-            db.People.Remove(person);
-            await db.SaveChangesAsync();
+            repo.Delete(person);
+            repo.SaveChanges();
 
             return NoContent();
-        }
-
-        private bool PersonExists(int id) {
-            return db.People.Any(e => e.Id == id);
         }
     }
 }
